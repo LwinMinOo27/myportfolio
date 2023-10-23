8646c7b86eb8c22305f678eec6d39253bcb3538d
lmo <21tyrantlmo@gmail.com>
2023-10-23 20:12:55 +0630

    change photo

--- src/components/Projects/data.jsx
+++ src/components/Projects/data.jsx
@@ -1,14 +1,14 @@
 export const project_data = [
   {
     id: 1,
-    vd_url: `travel.mp4`,
+    vd_url: `avatar.png`,
     title: `my travel journal`,
     time: `september 2023`,
     description: `This platform serves as an online hub specifically designed for travelers to share their travel experiences through written blogs.`,
   },
   {
     id: 2,
-    vd_url: `agency_vd.mp4`,
+    vd_url: `profile.jpg`,
     title: `whale agency`,
     time: `October 2023`,
     description: `Whale Seaman Agency is an online platform dedicated to connecting job seekers with maritime employment opportunities. It serves as a valuable resource for individuals in search of seaman positions, offering a streamlined approach to finding their next job in the maritime industry.`,
